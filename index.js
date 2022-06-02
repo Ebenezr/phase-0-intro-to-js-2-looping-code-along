ac706de0817f1c67552d5a2f726eac549c33c109
Ebenezr <ebenezarbukosia@gmail.com>
2022-06-02 12:33:15 +0300

    commit tree a0c9737c59275b9e69e3a7ccdc292e8d2175e4c7

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const cardMsg = [];
+const personNames = ["Guadalupe", "Ollie", "Aki"];
+function writeCards(personNames, eventName) {
+  for (let i = 0; i < personNames.length; i++) {
+    cardMsg.push(
+      `Thank you, ${personNames[i]}, for the wonderful ${eventName} gift!`
+    );
+  }
+  return cardMsg;
+}
+//writeCards(personNames, cardMsg);
+function countDown() {
+  let count = 10;
+  while (count >= 0) {
+    console.log(count--);
+  }
+}
